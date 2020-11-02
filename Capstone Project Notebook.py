1cbd77a047ffac1a23c110987ab47b50fc39cc01
rox215 <73795574+rox215@users.noreply.github.com>
2020-11-01 21:03:49 -0600

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+
+# This notebook will mostly be used for my Capstone Project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# Hello Capstone Project Course!
